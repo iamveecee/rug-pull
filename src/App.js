3d285cdbadc8826ada9c376fdd73b8907638bcbd
VinodChauhan <iamveecee@gmail.com>
2022-06-30 00:25:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import "./App.css";
 import Main from "./components/Main";
 import SwitchBtn from "./components/SwitchBtn";
 import SocialBtns from "./components/SocialBtns";
+import bgImgOff from "./assets/bg-off.png";
 
 function App() {
+
+    const mobileBgCSS = `@media (min-width: 320px) and (max-width: 767.98px)  {
+        .bg-light-on .main-bg-scene  {
+            background-image: url("../public/images/bg-mobile-on.png");
+        }
+    
+        .main-bg-scene {        
+            background-image: url("../public/images/bg-mobile-off.png");
+        }
+    }`;
+
+    const backgroundStyle = {
+        backgroundImage: `url(${bgImgOff})`,
+    }
     return (
         <>
+        <style scoped>{mobileBgCSS}</style>
+
             <main className="page-wrapper">
-                <div className=" main-bg-scene ">
+                <div className="main-bg-scene" style={backgroundStyle}>
                     <header className="header">
                         <nav className="navbar d-flex">
-                            <SocialBtns />
+                            <SocialBtns/>
                         </nav>
                     </header>
                     <SwitchBtn />
--- src/components/SwitchBtn.js
+++ src/components/SwitchBtn.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import bgImgOn from "../assets/bg-on.png";
+import bgImgOff from "../assets/bg-off.png";
 
 const SwitchBtn = () => {
-  return (
-    <div className="switch-btn" onClick={(e) =>{ document.body.classList.toggle('bg-light-on') }}></div>
-  )
-}
+    const elClick = (e) => {
+        const bgScreen = document.querySelector(".main-bg-scene");
+        if (document.body.classList.contains("bg-light-on")) {
+            document.body.classList.remove("bg-light-on");
+            bgScreen.style.backgroundImage = `url(${bgImgOff})`;
+        } else {
+            document.body.classList.add("bg-light-on");
+            bgScreen.style.backgroundImage = `url(${bgImgOn})`;
+        }
+    };
 
-export default SwitchBtn
+    return (
+        <div
+            className="switch-btn"
+            onClick={(e) => {
+                elClick(e);
+            }}
+        ></div>
+    );
+};
+
+export default SwitchBtn;
