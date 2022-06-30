8844f5fe5ab78cf5b27dbbc308670468a9cd55a0
VinodChauhan <iamveecee@gmail.com>
2022-07-01 00:06:35 +0530

    updated fix

--- src/components/SwitchBtn.js
+++ src/components/SwitchBtn.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const SwitchBtn = () => {
     const elClick = (e) => {
-      document.body.classList.toggle('bg-light-on') ;
+      document.body.classList.toggle('bg-light-on');
+       document.querySelector('.svg-cabinet').style.display="block";
+    
       /* 
         const bgScreen = document.querySelector(".main-bg-scene");
         if (document.body.classList.contains("bg-light-on")) {
