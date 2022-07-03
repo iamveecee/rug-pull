63b4dde8d2d61e7c373bc756496b8c566044f80b
VinodChauhan <iamveecee@gmail.com>
2022-07-03 23:34:31 +0530

    Glitch fix

--- src/components/SwitchBtn.js
+++ src/components/SwitchBtn.js
@@ -2,18 +2,13 @@ import React from "react";
 
 const SwitchBtn = () => {
     const elClick = (e) => {
-      document.body.classList.toggle('bg-light-on');
-       document.querySelector('.svg-cabinet').style.display="block";
-    
-      /* 
-        const bgScreen = document.querySelector(".main-bg-scene");
-        if (document.body.classList.contains("bg-light-on")) {
-            document.body.classList.remove("bg-light-on");
-            bgScreen.style.backgroundImage = `url(${bgImgOff})`;
-        } else {
-            document.body.classList.add("bg-light-on");
-            bgScreen.style.backgroundImage = `url(${bgImgOn})`;
-        } */
+        document.body.classList.toggle("bg-light-on");
+        document.querySelector(".svg-cabinet").style.display = "block";
+        document.querySelector(".main-heading svg").style.display = "block";
+        const mintSoon = document.querySelector(".mint-soon svg");
+        const mintBtn = document.querySelector(".mint-btn svg");
+        if (mintSoon !== null) mintSoon.style.display = "block";
+        if (mintBtn !== null) mintBtn.style.display = "block";
     };
 
     return (
